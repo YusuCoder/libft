9b4ccabb27999c5297d1cfc1d18996d6aeb4cd1f
Rustamjon Yusupov <ryusupov@2-E-4.42heilbronn.de>
2024-05-01 15:01:24 +0200

    Auto update script updating the submodules

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,13 +6,13 @@
 /*   By: ryusupov <ryusupov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 13:43:19 by ryusupov          #+#    #+#             */
-/*   Updated: 2024/04/28 17:04:24 by ryusupov         ###   ########.fr       */
+/*   Updated: 2024/05/01 14:47:54 by ryusupov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst)
 	{
